168d9011df77476cfc76761bca1a47b8671373d3
Mohsen-94 <34848846+Mohsen-94@users.noreply.github.com>
2019-07-27 11:49:51 +0300

    updated database url to work with github pages

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -7,8 +7,8 @@ class DBHelper {
    * Change this to restaurants.json file location on your server.
    */
   static get DATABASE_URL() {
-    const port = 8000; // Change this to your server port
-    return `http://localhost:${port}/data/restaurants.json`;
+//     const port = 8000; // Change this to your server port
+    return `https://mohsen-94.github.io/restaurant-reviews/data/restaurants.json`;
   }
 
   /**
